81548574aa31af9046eb5e1da3f9e104c9c711f4
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-11-08 17:52:17 +0600

    fix uwp mainpage

--- CodeHubX/PlatformSpecific/CodeHubX.Windows/CodeHubX.UWP/Views/MainPage.xaml.cs
+++ CodeHubX/PlatformSpecific/CodeHubX.Windows/CodeHubX.UWP/Views/MainPage.xaml.cs
@@ -1,13 +1,17 @@
-﻿using Xamarin.Forms.Platform.UWP;
+﻿using System.Reflection;
+using Windows.ApplicationModel.Activation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
 
 namespace CodeHubX.UWP.Views
 {
 	public sealed partial class MainPage
 	{
-		public MainPage()
+		public MainPage(IActivatedEventArgs args)
 		{
 			InitializeComponent();
 
+			Forms.Init(args, new[] { Assembly.Load("CodeHubX") });
 			LoadApplication(new CodeHubX.App());
 		}
 	}
