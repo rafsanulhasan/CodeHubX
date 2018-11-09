2fa3de262633f72280b597dc1ca15e2c5783b2c5
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-11-09 22:03:28 +0600

    simplify names

---
+++ PlatformSpecific/Linux/Android/Mobile/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace CodeHubX.Droid.Mobile
+{
+	[Activity(Label = "CodeHubX.Droid", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+			Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
