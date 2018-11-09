87b1eedc31b34537e7c18db74f8cfd4dc1caeda7
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-11-09 19:14:55 +0600

    Change Assembly title and copyright in common solution

--- PlatformSpecific/CodeHubX.Windows/CodeHubX.UWP/Properties/AssemblyInfo.cs
+++ PlatformSpecific/CodeHubX.Windows/CodeHubX.UWP/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeHubX.UWP")]
+[assembly: AssemblyTitle("CodeHubX for UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeHubX.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("CodeHubX")]
+[assembly: AssemblyCopyright("Copyright © SICS, 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- PlatformSpecific/CodeHubX.Windows/CodeHubX.WPF/Properties/AssemblyInfo.cs
+++ PlatformSpecific/CodeHubX.Windows/CodeHubX.WPF/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeHubX.WPF")]
+[assembly: AssemblyTitle("CodeHubX for Windwos Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeHubX.WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("CodeHubX")]
+[assembly: AssemblyCopyright("Copyright © SICS, 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
