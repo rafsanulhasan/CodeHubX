a347039f6f8d1b3018090e4243c6e112ae83e99e
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-11-14 16:03:10 +0600

    show appropiate content based on network connectivity

--- CodeHubX/App.xaml.cs
+++ CodeHubX/App.xaml.cs
@@ -6,7 +6,7 @@ using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using Microsoft.AppCenter.Push;
-using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,8 +15,12 @@ namespace CodeHubX
 {
 	public partial class App : Application
 	{
-		private Page mainPage;
-		private Page noNetworkPage;
+		private static MasterDetailPage mainPage;
+		private static NavigationPage noNetworkPage;
+		private static NavigationPage detailsPage;
+		private static NavigationPage currentPage;
+
+		private INetworkService _NetworkSvc;
 
 		//TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
 		public static string AzureBackendUrl = "http://localhost:5000";
@@ -37,22 +41,48 @@ namespace CodeHubX
 			L10n.SetLocale(ci);
 			Resources = L10n.LocalizeResource(Resources);
 
-			mainPage = new MainPage();
-			noNetworkPage = new NoNetworkPage();
+			DependencyService.Register<INetworkService, NetworkService>();
 
-			if (GlobalHelper.IsConnected)
-				MainPage = mainPage;
-			else
-				MainPage = noNetworkPage;
+			_NetworkSvc = DependencyService.Get<INetworkService>();
+
+			mainPage = new MainPage();
+			detailsPage = new NavigationPage(new ItemsPage());
+			noNetworkPage = new NavigationPage(new NoNetworkPage());
+
+			mainPage.Detail = _NetworkSvc.IsConnected ? detailsPage : noNetworkPage;
+			currentPage = new NavigationPage(mainPage.Detail);
+
+			MainPage = mainPage;
+
+			//MainPage = _NetworkSvc.IsConnected
+			//	    ? mainPage
+			//	    : noNetworkPage;
 		}
 
-		private void Current_ConnectivityChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e) 
-			=> MainPage = e.IsConnected ? mainPage : noNetworkPage;
+		private void ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+		{
+			GlobalHelper.IsConnected = e.IsConnected;
+			//if (e.IsConnected)
+			//{
+			//	currentPage = MenuPage.CurrentPage;
+			//}
+			//else
+			//{
+			//	detailsPage = new NavigationPage(mainPage.Detail);				
+			//	currentPage = noNetworkPage;
+			//}
+			mainPage.Detail = e.IsConnected ? MenuPage.CurrentPage : new NavigationPage(new NoNetworkPage());
+		}
+
+		private void ConnectionTypeChanged(object sender, ConnectivityTypeChangedEventArgs e)
+		{
+
+		}
 
 		protected override void OnStart()
 		{
-
-			CrossConnectivity.Current.ConnectivityChanged += Current_ConnectivityChanged;
+			_NetworkSvc.ConnectivityChanged += ConnectivityChanged;
+			_NetworkSvc.ConnectionTypeChanged += ConnectionTypeChanged;
 
 			// Handle when your app starts
 			AppCenter.Start("ios=0d2abada-0d0b-43c3-a500-832f8016b21d;android=b6f7aef7-c910-4c0e-a302-61b1f8095e9d;uwp=70adf665-fc63-4a9c-880f-b390818e93b5", typeof(Push), typeof(Analytics), typeof(Crashes));
@@ -60,11 +90,15 @@ namespace CodeHubX
 
 		protected override void OnSleep()
 		{
+			_NetworkSvc.ConnectivityChanged -= ConnectivityChanged;
+			_NetworkSvc.ConnectionTypeChanged -= ConnectionTypeChanged;
 			// Handle when your app sleeps
 		}
 
 		protected override void OnResume()
 		{
+			_NetworkSvc.ConnectivityChanged += ConnectivityChanged;
+			_NetworkSvc.ConnectionTypeChanged += ConnectionTypeChanged;
 			// Handle when your app resumes
 		}
 	}
---
+++ CodeHubX/Services/CustomDependencyService.cs
@@ -0,0 +1,49 @@
+﻿using DS = Xamarin.Forms.DependencyService;
+
+namespace CodeHubX.Services
+{
+	public class CustomDependencyService
+	{
+		public static void Register<T>(T param)
+			where T : class
+		{
+			DS.Register<T>();
+			var t = param.GetType();
+			var propInfo = typeof(T).GetProperty(t.Name, t);
+			var obj = DS.Get<T>();
+			propInfo.SetValue(obj, param);
+		}
+
+		public static void Register<T, TParam>(T instance, TParam param)
+			where T : class
+		{
+			DS.Register<T>();
+			var t = param.GetType();
+			var propInfo = typeof(T).GetProperty(t.Name, t);
+			var obj = DS.Get<T>();
+			propInfo.SetValue(obj, param);
+		}
+
+		public static void Register<T, TImpl, TParam>(TParam param)
+			where T : class
+			where TImpl : class, T
+		{
+			DS.Register<T, TImpl>();
+			var t = param.GetType();
+			var propInfo = typeof(T).GetProperty(t.Name, t);
+			var obj = DS.Get<T>();
+			propInfo.SetValue(obj, param);
+		}
+
+		public static void Register<T, TImpl, TParam>(TImpl instance, TParam param)
+			where T : class
+			where TImpl : class, T
+		{
+			DS.Register<T, TImpl>();
+			var t = param.GetType();
+			var propInfo = typeof(T).GetProperty(t.Name, t);
+			var obj = DS.Get<T>();
+			propInfo.SetValue(obj, param);
+		}
+	}
+}
