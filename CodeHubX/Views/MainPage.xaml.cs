34b5435788f30e1ede292b0860ecc9441d738f90
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-11-14 16:03:47 +0600

    cleanup shared project file

--- CodeHubX/Views/MainPage.xaml.cs
+++ CodeHubX/Views/MainPage.xaml.cs
@@ -4,19 +4,15 @@ using Xamarin.Forms.Xaml;
 namespace CodeHubX.Views
 {
 	using Services;
-	using static Helpers.GlobalHelper;
-
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : MasterDetailPage
 	{
-
 		public MainPage()
 		{
 			InitializeComponent();
 
 			MasterBehavior = MasterBehavior.Popover;
 
-			//_MenuPages.Add(0, (NavigationPage)Detail);
 			MenuService.Add(0, (NavigationPage) Detail);
 		}
 	}
