54d3bb827e8a61e934362b07d68144f228ee95fb
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-11-14 15:58:34 +0600

    updated navigation code

--- CodeHubX/Views/MenuPage.xaml.cs
+++ CodeHubX/Views/MenuPage.xaml.cs
@@ -1,38 +1,48 @@
-﻿using CodeHubX.Models;
+﻿
 using System.Collections.Generic;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CodeHubX.Views
 {
+	using Models;
+	using Services;
+
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MenuPage : ContentPage
 	{
-		private MainPage RootPage => Application.Current.MainPage as MainPage;
+		//private MainPage RootPage => Application.Current.MainPage as MainPage;
+		public static NavigationPage CurrentPage;
 
-		private List<HomeMenuItem> menuItems;
+		private IList<MenuItem> _MenuItems;
+		//private AboutPage _AboutPage=new AboutPage();
+		//private Ite
+		//private FeedsPage _FeedsPage=new FeedsPage();
+
 		public MenuPage()
 		{
 			InitializeComponent();
 
-			menuItems = new List<HomeMenuItem>
+			_MenuItems = _MenuItems ?? new List<MenuItem>
 			{
-				new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
-				new HomeMenuItem {Id = MenuItemType.About, Title="About" },
-				new HomeMenuItem {Id = MenuItemType.Feeds, Title = "Feeds" }
+				new MenuItem {Id = MenuItemType.Browse, MenuTitle = "Browse", PageTitle = "Browse" },
+				new MenuItem {Id = MenuItemType.About, MenuTitle = "About", PageTitle = "About us" },
+				new MenuItem {Id = MenuItemType.Feeds, MenuTitle = "Feeds", PageTitle = "Latest Feeds" }
 			};
 
-			ListViewMenu.ItemsSource = menuItems;
+			ListViewMenu.ItemsSource = _MenuItems;
 
-			ListViewMenu.SelectedItem = menuItems[0];
+			ListViewMenu.SelectedItem = _MenuItems[0];
+			CurrentPage = MenuService.Get((ListViewMenu.SelectedItem as MenuItem).Number);
 			ListViewMenu.ItemSelected += async (sender, e) =>
 			{
 				if (e.SelectedItem == null)
 					return;
-
-				var id = (int) ((HomeMenuItem) e.SelectedItem).Id;
-				await RootPage.NavigateFromMenu(id);
+				var selectedMenu = (MenuItem) e.SelectedItem;
+				CurrentPage = MenuService.Get(selectedMenu.Number);
+				await NavigationService.NavigateAsync(selectedMenu.Number);
+				//var id = (int) ((MenuItem) e.SelectedItem).Id;
+				//await RootPage.NavigateFromMenu(id);
 			};
 		}
 	}
